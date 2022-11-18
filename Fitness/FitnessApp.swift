b646b62309880683e0928d30561735b91f62719d
Duff Neubauer <d.neubauer@me.com>
2022-11-18 07:17:31 -0800

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Duff Neubauer on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
